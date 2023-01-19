addb797066ef0c33969449f6fa4664b62f3708b5
am533 <44439008+am533@users.noreply.github.com>
2023-01-19 07:18:46 -0500

    Update package-info.java

--- Tic-Tac-Toe/src/controller/package-info.java
+++ Tic-Tac-Toe/src/controller/package-info.java
@@ -1,11 +1,12 @@
 /**
  * The controller package contains all the classes responsible for managing the
- * interactions between the user, the model and the view of the Tic-Tac-Toe
+ * interactions between the user, the model, and the view of the Tic-Tac-Toe
  * game. It includes the GUI and UIBoardUtilities classes. These classes work
- * together to handle user input (button clicking), game flow, and alert the
+ * together to handle user input (button clicking) and game flow and alert the
  * user of wins, ties, and score updates using the data and logic defined in the
- * model package and the user interface (UI) elements and CSS styling defined in
- * the view package. Overall, this package is responsible for providing the game
- * flow and control needed for the GUI of the game to function properly.
+ * model package. It displays these controls and defines their actions using the
+ * user interface (UI) elements and CSS styling defined in the view package.
+ * Overall, this package is responsible for providing the game flow and control
+ * needed for the GUI of the game to function properly.
  */
 package controller;
