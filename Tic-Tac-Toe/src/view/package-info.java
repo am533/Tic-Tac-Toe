6b3407f47aeebbea3cc05756f3c5907cc54781bc
am533 <44439008+am533@users.noreply.github.com>
2023-01-19 07:26:13 -0500

    Update package-info.java

--- Tic-Tac-Toe/src/view/package-info.java
+++ Tic-Tac-Toe/src/view/package-info.java
@@ -2,9 +2,9 @@
  * The view package contains all the classes related to the display of the
  * graphical user interface of the Tic-Tac-Toe game. It includes classes such as
  * UIElements, CSSStyles, and UIConstants which handle the creation and styling
- * of user interface (UI) elements such as buttons, labels and the game board.
- * This package also contains classes that define the CSS styles used such as
- * background color and text color, and constants such as dimensions and font
+ * of user interface (UI) elements such as buttons, labels, and the game board.
+ * This package also contains classes that define the CSS styles used, such as
+ * background color and text color, and constants, such as dimensions and font
  * sizes for all the different UI elements. Overall, this package is responsible
  * for providing the tools to make a visually appealing experience for the user
  * while playing the game.
